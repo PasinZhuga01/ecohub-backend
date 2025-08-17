8a963ecaccb59305d3620524484e86b71a7d61c3
PasinZhuga01 <sergijumanskij@gmail.com>
2025-08-17 20:30:14 +0300

    feat(models): update ModelsUtility, add centralized sending sql requests and remove old models

--- src/models/utility.ts
+++ src/models/utility.ts
@@ -1,50 +1,85 @@
 import { z } from 'zod';
 
+import { IdentifiedObject } from './facade';
+
 import db from '../config/db';
 import { DatabaseError } from '../errors';
 
-export class ModelsUtility<T extends z.ZodType> {
+export class ModelsUtility<T extends z.ZodType<IdentifiedObject, IdentifiedObject>, C extends keyof z.input<T>> {
 	private readonly _schema: T;
-	private readonly _itemName: string;
 
-	public constructor(schema: T, itemName: string) {
+	private readonly _tableName: string;
+	private readonly _tableItemName: string;
+
+	public constructor(schema: T, tableName: string, tableItemName: string) {
 		this._schema = schema;
-		this._itemName = itemName;
+		this._tableName = tableName;
+		this._tableItemName = tableItemName;
 	}
 
-	public async getEntity(sql: string, values: unknown[]): Promise<z.output<T> | null> {
-		const [rows] = await db.query(sql, values);
-
-		return Array.isArray(rows) && rows.length > 0 ? this._schema.parse(rows[0]) : null;
+	public async getEntity(data: Partial<z.input<T>>): Promise<z.output<T> | null> {
+		return (await this.getEntities(data, { count: 1 }))[0] ?? null;
 	}
 
-	public async getEntities(sql: string, values: unknown[]): Promise<z.output<T>[]> {
-		const [rows] = await db.query(sql, values);
+	public async getEntities(
+		data: Partial<z.input<T>>,
+		options: Partial<{ orderKey: keyof z.input<T>; count: number }> = {}
+	): Promise<z.output<T>[]> {
+		const where = ModelsUtility._toSqlClause(data);
+		const values = Object.values(data);
+		const limit = options.count !== undefined && isFinite(options.count) ? 'LIMIT ?' : '';
+		const order = options.orderKey !== undefined ? `ORDER BY ${String(options.orderKey)} DESC` : '';
+
+		if (options.count !== undefined) {
+			values.push(options.count);
+		}
+
+		const [rows] = await db.query(`SELECT * FROM ${this._tableName} WHERE ${where} ${order} ${limit}`, values);
 
 		if (!Array.isArray(rows)) {
-			throw new DatabaseError(`Failed to retrieve ${this._itemName} list`);
+			throw new DatabaseError(`Failed to retrieve ${this._tableItemName} list`);
 		}
 
 		return rows.map((row) => this._schema.parse(row));
 	}
 
-	public async createEntity(
-		sql: string,
-		values: unknown[],
-		getCreatedEntity: (id: number) => Promise<z.output<T> | null>
-	): Promise<z.output<T>> {
-		const [result] = await db.execute(sql, values);
+	public async createEntity(data: Pick<z.input<T>, C>): Promise<z.output<T>> {
+		const keys = Object.keys(data).join(', ');
+		const values = Object.values(data);
+		const inserts = new Array(values.length).fill('?').join(', ');
+
+		const [result] = await db.execute(`INSERT INTO ${this._tableName} (${keys}) VALUES (${inserts})`, values);
 
 		if (!('insertId' in result) || typeof result.insertId !== 'number') {
-			throw new DatabaseError(`Failed to retrieve created ${this._itemName} insertId`);
+			throw new DatabaseError(`Failed to retrieve created ${this._tableItemName} insertId`);
 		}
 
-		const entity = await getCreatedEntity(result.insertId);
+		const entity = await this.getEntity({ id: result.insertId } as Partial<z.input<T>>);
 
 		if (entity === null) {
-			throw new DatabaseError(`Failed to retrieve created ${this._itemName} with id = ${result.insertId}`);
+			throw new DatabaseError(`Failed to retrieve created ${this._tableItemName} with id = ${result.insertId}`);
 		}
 
 		return entity;
+	}
+
+	public async updateEntity(data: Partial<z.input<T>>) {
+		const set = ModelsUtility._toSqlClause(data);
+		const values = Object.values(data);
+
+		await db.execute(`UPDATE ${this._tableName} SET ${set}`, values);
+	}
+
+	public async removeEntity(data: Partial<z.input<T>>) {
+		const where = ModelsUtility._toSqlClause(data);
+		const values = Object.values(data);
+
+		await db.execute(`DELETE FROM ${this._tableName} WHERE ${where}`, values);
+	}
+
+	private static _toSqlClause(data: object): string {
+		return Object.keys(data)
+			.map((key) => `${key} = ?`)
+			.join(' AND ');
 	}
 }
