ae3828a7f058fc0a2744874dc1e5b9f6e597cc3a
PasinZhuga01 <sergijumanskij@gmail.com>
2025-08-17 18:04:33 +0300

    feat(models): create currencies models

---
+++ src/models/currencies.models.schemas.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+import { currency as currencyBaseSchema } from './facade';
+
+export const currencySchema = currencyBaseSchema.transform(({ project_id, icon_src, ...object }) => ({
+	...object,
+	projectId: project_id,
+	iconSrc: icon_src
+}));
+
+export type CurrencyObject = z.output<typeof currencySchema>;
