4a0819fdd44c1b0e37aefcdead9650556ca66856
PasinZhuga01 <sergijumanskij@gmail.com>
2025-08-19 01:48:48 +0300

    refactor(utils): use response from types instead of express

--- src/controllers/utils.ts
+++ src/controllers/utils.ts
@@ -1,10 +1,9 @@
-import { Response } from 'express';
+import { Response } from './types';
 
 import { PayloadError } from '../errors';
 import { ErrorCodes } from '../constants/http';
-import { ErrorPayload } from '../types/http';
 
-export async function safePayload<T extends object>(res: Response<T | ErrorPayload>, callback: () => Promise<T>) {
+export async function safePayload<T extends object>(res: Response<T>, callback: () => Promise<T>) {
 	try {
 		res.json(await callback());
 	} catch (error) {
