bb37ddcc538fc9ac1ff6018fce66c76cd3e9d2ed
PasinZhuga01 <sergijumanskij@gmail.com>
2025-08-18 23:10:26 +0300

    feat(utils): remove utils

--- src/utils/http.ts
+++
@@ -1,6 +0,0 @@
-import { ErrorCodes } from '../constants/http';
-import { ErrorPayload, ErrorResponse } from '../types/http';
-
-export function getErrorResponse(payload: ErrorPayload): ErrorResponse {
-	return { status: ErrorCodes[payload.code], ...payload };
-}
