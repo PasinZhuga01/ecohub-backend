134943f1e927ca3566a0d485d0da1cb964f0096a
PasinZhuga01 <sergijumanskij@gmail.com>
2025-08-19 05:32:06 +0300

    feat(app): assemble project

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 
+import router from './routes';
 import env from './config/env';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors({ origin: env.clientUrl }));
+
+app.use('/', router);
 
 export default app;
